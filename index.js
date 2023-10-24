32295e819a5a83835c7e79d84b746993a4a51926
RoflKartoffelDE <wuensche.e@gmail.com>
2023-10-24 23:34:47 +0200

    fix check of link

--- index.js
+++ index.js
@@ -567,71 +567,12 @@
       });
   }
 
-  async function getPaginatedData(url) {
-    const nextPattern = /(?<=<)([\S]*)(?=>; rel="Next")/i;
-    let pagesRemaining = true;
-    let data = [];
-    const linkHeader = response.headers.link;
-
-    pagesRemaining = linkHeader && linkHeader.includes(`rel=\"next\"`);
-
-    if (pagesRemaining) {
-      url = linkHeader.match(nextPattern)[0];
-    }
-
-    while (pagesRemaining) {
-      const response = await octokit.request(`GET ${url}`, {
-        per_page: 100,
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      });
-
-      const parsedData = parseData(response.data);
-      data = [...data, ...parsedData];
-
-      const linkHeader = response.headers.link;
-
-      pagesRemaining = linkHeader && linkHeader.includes(`rel=\"next\"`);
-
-      if (pagesRemaining) {
-        url = linkHeader.match(nextPattern)[0];
-      }
-    }
-
-    return data;
-  }
-
-  function parseData(data) {
-    // If the data is an array, return that
-    if (Array.isArray(data)) {
-      return data;
-    }
-
-    // Some endpoints respond with 204 No Content instead of empty array
-    //   when there is no data. In that case, return an empty array.
-    if (!data) {
-      return [];
-    }
-
-    // Otherwise, the array of items that we want is in an object
-    // Delete keys that don't include the array of items
-    delete data.incomplete_results;
-    delete data.repository_selection;
-    delete data.total_count;
-    // Pull out the array of items
-    const namespaceKey = Object.keys(data)[0];
-    data = data[namespaceKey];
-
-    return data;
-  }
-
   function getRepos(link) {
     setStatusIndicatorLoading(
       document.getElementById("status_indicator_repository")
     );
     let apiEndPoint = "/user/repos";
-    if (link && link instanceof String) {
+    if (link && !(link instanceof Object)) {
       apiEndPoint = link;
     } else {
       document.forms.githubSetup.repository.innerHTML = "";
@@ -702,7 +643,7 @@
     let owner = document.forms.githubSetup.repository.value.split(";", 1)[0];
     let repo = document.forms.githubSetup.repository.value.split(";", 2)[1];
     let apiEndPoint = `/repos/${owner}/${repo}/branches`;
-    if (link && link instanceof String) {
+    if (link && !(link instanceof Object)) {
       apiEndPoint = link;
     } else {
       document.forms.githubSetup.branch.innerHTML = "";
